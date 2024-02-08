acc7210fc927bd3cd8a1527fb6f65d58e54d2a8a
Lucas dos Anjos <lucas.anjos@bizsys.com.br>
2024-02-08 17:07:07 -0300

    update projeto aula 02 Funções

--- F1 - Iniciante em Programação/logica-js-projeto2/app.js
+++ F1 - Iniciante em Programação/logica-js-projeto2/app.js
@@ -1,7 +1,26 @@
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número de 1 a 10:';
-
-
-function verificarChute() {
-    console.log('Btn chute foi CLICADO!');
+function innerTextElement(elemento, texto) {
+    let element = document.querySelector(elemento);
+    element.innerHTML = texto;
 }
+
+function gerarNumAleatorio() {
+    return Math.floor(Math.random() * 10 + 1);
+}
+
+innerTextElement('p', 'Escolha um número de 1 a 10:');
+
+let numeroSecreto = gerarNumAleatorio();
+// console.log(numeroSecreto);
+
+
+function onVerificarChute() {
+    let inputChute = parseInt(document.querySelector('input').value); // pega o valor do input
+    
+    if(isNaN(inputChute)) {
+        alert('Preencha o campo!');
+    } else {
+        console.log(inputChute);
+        console.log(inputChute === numeroSecreto);
+    }
+}
+
