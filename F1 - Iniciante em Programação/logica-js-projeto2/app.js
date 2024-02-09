8855b1f188381c01f179805f4cd1376fcf9751f1
Lucas dos Anjos <lucas.anjos@bizsys.com.br>
2024-02-09 17:59:15 -0300

    update projeto 2 (falta ajustes finais)

--- F1 - Iniciante em Programação/logica-js-projeto2/app.js
+++ F1 - Iniciante em Programação/logica-js-projeto2/app.js
@@ -2,25 +2,48 @@ function innerTextElement(elemento, texto) {
     let element = document.querySelector(elemento);
     element.innerHTML = texto;
 }
-
-function gerarNumAleatorio() {
-    return Math.floor(Math.random() * 10 + 1);
+function telaInicial() {
+    innerTextElement('h1', 'Jogo do número secreto');
+    innerTextElement('p', 'Escolha um número de 1 a 10:');
 }
+telaInicial();
 
-innerTextElement('p', 'Escolha um número de 1 a 10:');
+function gerarNumAleatorio(max) {
+    return Math.floor(Math.random() * max + 1);
+}
+let numeroSecreto = gerarNumAleatorio(3);
+console.log(numeroSecreto);
+let tentativa = 1;
 
-let numeroSecreto = gerarNumAleatorio();
-// console.log(numeroSecreto);
 
 
 function onVerificarChute() {
-    let inputChute = parseInt(document.querySelector('input').value); // pega o valor do input
+    let inputChute = document.querySelector('input').value; // pega o valor do input
     
-    if(isNaN(inputChute)) {
-        alert('Preencha o campo!');
+    if(inputChute == '') {
+        innerTextElement('p', 'Escolha um número de 1 a 10: (preencha o campo!)');
+        //estlizar input red + animar
+    } else if(inputChute == numeroSecreto) {
+        innerTextElement('h1', 'Acertou!');
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativa} ${tentativa == 1 ? 'tentativa' : 'tentativas'}`;
+        innerTextElement('p', mensagemTentativas);
+        //habiliar botão Novo jogo 
+        //desabilita chute
+        //fazerfynca de desabilitar
     } else {
-        console.log(inputChute);
-        console.log(inputChute === numeroSecreto);
+        tentativa ++;
+
+        if(inputChute > numeroSecreto) {
+            innerTextElement('p', 'O número secreto é menor!');
+        } else {
+            innerTextElement('p', 'O número secreto é maior!');
+        }
+        //limpara campo? acho melhor animar input
     }
+}
+
+function onReiniciaJogo() {
+    telaInicial();
+    // outras coisa...
 }
 
