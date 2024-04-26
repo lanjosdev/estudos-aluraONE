da89e0ac884a39473692ad4df7b92afa3be66e09
Lucas dos Anjos <lucas.anjospro@gmail.com>
2024-04-26 12:37:58 -0300

    projeto Alura midi finalizado

--- F2 - Formacao Front End/2_JsWeb-CriePagesDinamicas/main.js
+++ F2 - Formacao Front End/2_JsWeb-CriePagesDinamicas/main.js
@@ -1,18 +1,36 @@
 // Variaveis / Elementos da UI:
 const teclas = document.querySelectorAll('.tecla');
-// const audios = document.querySelectorAll('audio');
 
 // Funções:
 function tocaSom(idAudio) {
-    document.querySelector(idAudio).play();
+    const elementoAudio = document.querySelector(idAudio);
+
+    // Validação antes de reproduzir audio:
+    // 'localName' é p/ especificar qual é o tipo do elemento/tag
+    if(elementoAudio && elementoAudio.localName === 'audio') {
+        elementoAudio.play();
+    }
+    else {
+        console.error('Elemento não encontrado OU elemento não é do tipo audio.');
+    }
 }
 
 
 // Eventos da UI:
-// teclaPom.onclick = ()=> tocaSom('#som_tecla_pom');
 for(let i = 0; i < teclas.length; i++) {
-    let classIntrumento = teclas[i].classList[1];
-    let idAudio = `#som_${classIntrumento}`;
+    let classInstrumento = teclas[i].classList[1];
+    let idAudio = `#som_${classInstrumento}`;
 
+    // evento click do mouse:
     teclas[i].onclick = ()=> tocaSom(idAudio);
+
+    // eventos com teclado:
+    teclas[i].onkeydown = (event)=> {
+        if(event.code === 'Enter' || event.code === 'NumpadEnter' || event.code === 'Space') {
+            teclas[i].classList.add('ativa', 'active');
+        }
+    }
+    teclas[i].onkeyup = (event)=> {
+        teclas[i].classList.remove('ativa', 'active');
+    }
 }
