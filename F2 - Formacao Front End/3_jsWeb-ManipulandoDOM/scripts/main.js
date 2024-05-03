61346f854393f442bca11a3db3132dd05e78a4be
Lucas dos Anjos <lucas.anjospro@gmail.com>
2024-05-02 22:40:11 -0300

    feito alternacia de contexto + ativar btn modos

--- F2 - Formacao Front End/3_jsWeb-ManipulandoDOM/scripts/main.js
+++ F2 - Formacao Front End/3_jsWeb-ManipulandoDOM/scripts/main.js
@@ -6,23 +6,56 @@ const btnFoco = document.querySelector('.app__card-button--foco');
 const btnDesCurto = document.querySelector('.app__card-button--curto');
 const btnDesLongo = document.querySelector('.app__card-button--longo');
 
+const titulo = document.querySelector('.app__title');
 // Imgs
-const bannner =document.querySelector('.app__image');
+const bannner = document.querySelector('.app__image');
 
 // FUNÇÔES:
 function alterarContexto(contexto) {
+    body.setAttribute('data-contexto', contexto);
+
+    bannner.setAttribute('src', `./imagens/${contexto}.png`);
     
+    switch(contexto) {
+      case 'foco':
+        titulo.innerHTML = `Otimize sua produtividade,<br>
+          <strong class="app__title-strong">mergulhe no que importa.
+          </strong>`;
+        break;
+      case 'descanso-longo':
+        titulo.innerHTML = `Hora de voltar à superfície. <br>
+          <strong class="app__title-strong">
+            Faça uma pausa longa
+          </strong>`;
+        break;
+      case 'descanso-curto':
+        titulo.innerHTML = `Que tal dar uma respirada?<br>
+          <strong class="app__title-strong">
+            Faça uma pausa curta!
+          </strong>`;
+        break;
+      default:
+        break;
+    }
 }
 
+function ativarBtn(elemento) {
+    document.querySelectorAll('.app__card-button')
+        .forEach(btn => btn.classList.remove('active'));
+    elemento.classList.add('active');
+}
+
+
+// EVENTOS/INTERATIVIDADE UI:
 btnFoco.addEventListener('click', ()=> {
-    body.setAttribute('data-contexto', 'foco');
-    bannner.setAttribute('src', './imagens/foco.png');
+    alterarContexto('foco');
+    ativarBtn(btnFoco);
 });
 btnDesCurto.addEventListener('click', ()=> {
-    body.setAttribute('data-contexto', 'descanso-curto');
-    bannner.setAttribute('src', './imagens/descanso-curto.png');
+    alterarContexto('descanso-curto');
+    ativarBtn(btnDesCurto);
 });
 btnDesLongo.addEventListener('click', ()=> {
-    body.setAttribute('data-contexto', 'descanso-longo');
-    bannner.setAttribute('src', './imagens/descanso-longo.png');
+    alterarContexto('descanso-longo');
+    ativarBtn(btnDesLongo);
 });
